dd8dcf13851cc7d51f1d5fdcd36f209be9061b48
Gordon Brander <gordonbrander@users.noreply.github.com>
2015-06-19 15:38:03 -0700

    Update markdown plugin to use mapping

--- lettersmith/markdown.lua
+++ lettersmith/markdown.lua
@@ -3,10 +3,9 @@ Lettersmith Markdown
 Renders markdown in contents field.
 --]]
 local markdown = require("discount")
-local map = require("lettersmith.transducers").map
-local transformer = require("lettersmith.reducers").transformer
+local mapping = require("lettersmith.reducers").mapping
 local renderer = require("lettersmith.plugin_utils").renderer
 
-local render_markdown = transformer(map(renderer(markdown)))
+local render_markdown = mapping(renderer(markdown))
 
 return render_markdown
