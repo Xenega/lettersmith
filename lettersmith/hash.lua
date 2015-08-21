76b1a748db87ef297aeebc37a8499bdb8f432f05
Gordon Brander <gordonbrander@users.noreply.github.com>
2015-08-21 08:58:40 -0700

    Update hashing plugin

--- lettersmith/hash.lua
+++ lettersmith/hash.lua
@@ -5,11 +5,10 @@ Returns a transformer that fills in the 'hash' metadata field of a docs iterator
 This field contains the 32 hexadecimal digits of the md5sum of the document contents.
 Fields from document take precedence.
 --]]
-local map = require("lettersmith.transducers").map
-local transformer = require("lettersmith.reducers").transformer
+local mapping = require("lettersmith.plugin_utils").mapping
 local extend = require("lettersmith.table_utils").extend
 local md5sum = require("md5").sumhexa
 
-return transformer(map(function(doc)
-  return extend({ hash=md5sum(doc.contents) }, doc)
-end))
+return mapping(function(doc)
+  return extend({ hash = md5sum(doc.contents) }, doc)
+end)
