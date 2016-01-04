284e7a949250fb81b8e2eb9a72e8fcc4a5d1dd69
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-01-03 18:16:29 -0800

    Export plugin function for drafts

--- lettersmith/drafts.lua
+++ lettersmith/drafts.lua
@@ -7,14 +7,14 @@ an underscore.
 local filtering = require("lettersmith.plugin_utils").filtering
 local path_utils = require("lettersmith.path_utils")
 
-local function is_doc_path_prefixed_with_underscore(doc)
+local function isnt_prefixed_with_underscore(doc)
   -- Treat any document path that starts with an underscore as a draft.
-  return path_utils.basename(doc.relative_filepath):find("^_")
+  return path_utils.basename(doc.relative_filepath):find("^_") == nil
 end
 
 -- Remove all docs who's path is prefixed with an underscore.
-local remove_drafts = filtering(function (doc)
-  return not is_doc_path_prefixed_with_underscore(doc)
-end)
+local function remove_drafts()
+  return filtering(isnt_prefixed_with_underscore)
+end
 
 return remove_drafts
