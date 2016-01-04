a870066392fc14516ceeac4951fcbb2f94c62531
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-01-03 19:04:33 -0800

    Export plugin function for markdown

--- lettersmith/markdown.lua
+++ lettersmith/markdown.lua
@@ -5,6 +5,8 @@ Renders markdown in contents field.
 local markdown = require("discount")
 local rendering = require("lettersmith.plugin_utils").rendering
 
-local render_markdown = rendering(markdown)
+local function render_markdown()
+  return rendering(markdown)
+end
 
 return render_markdown
